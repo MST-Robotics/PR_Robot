0b79abdd7b75d36c844fb295b831173c938a022c
ctorralba <ctorralba0@gmail.com>
2017-11-02 00:23:49 -0500

    Added random lighting functionality (Harder Mode)
    - Press back on controller to toggle between random lighting (individual
    LEDs are turned to HIGH or LOW) and having all LEDs on.
    
    - Added a default variable (randomOn = 1) to start out with random lighting,
    otherwise turn on all LEDs (randomOn = 0).
    
    - Time frequency between random LED changing is determined randomly between
    5-10 seconds.
    
    -Still untested but compiles

--- prRobot/prRobot.ino
+++ prRobot/prRobot.ino
@@ -4,6 +4,8 @@
 
 #include <Servo.h>
 #include <XBOXRECV.h>
+#include <time.h>
+#include <stdlib.h>
 
 USB Usb;
 XBOXRECV Xbox(&Usb);
@@ -34,11 +36,19 @@ Servo claw;  //servo controlling claw movements, assume only one servo needed ->
 #define ELBOW_PIN 3
 #define CLAW_PIN 4
 
+
 //led constants
 //placeholder pin values for all LEDs
 const unsigned char LED_PINS[] = {1,2,3,4,5,6,7,8,9};//The LED_PINS array; can add more arrays to seperate LEDs for different goal posts.
-char led_pin_array_size = sizeof(LED_PINS)/sizeof(LED_PINS[0]); //size of the LED_PINS array
-const unsigned int LED_DELAY = 1000; //time between leds turning on (ms)
+const unsigned char led_pin_array_size = sizeof(LED_PINS)/sizeof(LED_PINS[0]); //size of the LED_PINS array
+
+//random lighting led variables
+unsigned char gameStarted = 0;//(0 = start button has not been pressed, 1 = start has been pressed)
+                              //used so random LEDs start only when start button has been pressed
+unsigned char randomOn = 0;   //default option for random LED lighting (0 = all leds on, 1 = start with random lighting)
+unsigned long current_time = 0, previous_time = 0, interval= 5000; //used to determine random LED changes
+                                                                   //interval is initally set, it changes
+                                                                   //randomly between 5-9 sec within loop
 
 int moveSpeed = 2;
 //thses are temp values
@@ -67,10 +77,26 @@ void setup() {
   for(unsigned char i = 0; i < led_pin_array_size-1;i++){
     pinMode(LED_PINS[i], OUTPUT);
   }
+  
+  //time seed set to NULL - used for random LEDs
+  srand(time(NULL));
 }
 
 void loop() {
   Usb.Task();  //"polls connected usb devices for updates to their status"
+
+  //Random LED Lighting
+  //randomOn is initally set above (@variable declerations) or toggled using 'BACK' button
+  //gameStarted is set when start button has been pressed
+  if (randomOn && gameStarted){
+    current_time = millis();
+    if (current_time - previous_time >= interval){//if time difference since last change is greater than interval set
+      randomLEDs();                               //change LEDs individually to HIGH or LOW
+      previous_time = current_time;               //record time for last change
+      interval = rand() % 5000 + 5000;            //change the interval between 5-10 seconds
+    }
+  }
+  
   if (Xbox.XboxReceiverConnected) {
     for(uint8_t i = 0; i < 4; i++) //for each possible controller
     {
@@ -133,7 +159,6 @@ void loop() {
             pos_elbow = LOWER_BOUND; 
           elbow.write(pos_elbow); //sets servo position
         }
-
         
         //close the claw
         if(Xbox.getButtonClick(L2, i)) {
@@ -146,8 +171,24 @@ void loop() {
           claw.write(POS_CLAW_OPEN); //sets servo position
         }
          //if start button gets triggered
-        if (Xbox.getButtonClick(START, i))
-          turnOnAllLED(); //turns on all LED's
+        if (Xbox.getButtonClick(START, i)){
+          gameStarted = 1;
+          //if randomOn is set it initializes LEDs to random LED lighting else it turns on all LEDs
+          (randomOn)? randomLEDs():turnOnAllLEDs();
+        }
+
+        //if back button is pressed it toggles between random LED lighting and having all LEDs on
+        if (Xbox.getButtonClick(BACK, i)){
+          if (randomOn){ //change to not have random LED lighting timing
+            randomOn = 0;
+            turnOnAllLEDs();
+          }
+          else{  //change to random LED lighting timing
+            randomOn = 1;
+            randomLEDs();
+          }
+        }
+
       }
     }
   }
@@ -159,10 +200,9 @@ void loop() {
  * @Param None, xbox start button has been pressed
  * @Return All LED specified should turn on.
  */
-void turnOnAllLED(){
+void turnOnAllLEDs(){
   for(unsigned char i = 0; i < led_pin_array_size-1;i++){
     digitalWrite(LED_PINS[i], HIGH);
-    delay(LED_DELAY);
   }
 }
 
@@ -171,9 +211,22 @@ void turnOnAllLED(){
  * @Param None
  * @Return All LED specified should turn off.
  */
-void turnOffAllLED(){
+void turnOffAllLEDs(){
   for(unsigned char i = 0; i < led_pin_array_size-1;i++){
     digitalWrite(LED_PINS[i], LOW);
-    delay(LED_DELAY);
+  }
+}
+
+/**
+ * Random LEDs are turned on and others off
+ * @Param randomOn has been set(1)
+ * @Return All LED are changed to either HIGH or LOW randomly
+ */
+void randomLEDs(){
+  unsigned char set_or_unset;
+  for(unsigned char i = 0; i < led_pin_array_size-1;i++){
+    set_or_unset = rand()%2; //0 or 1
+    //if set_or_unset is 1 then that LED is set to HIGH otherwise set to LOW
+    set_or_unset? digitalWrite(LED_PINS[i], HIGH):digitalWrite(LED_PINS[i], LOW);
   }
 }
